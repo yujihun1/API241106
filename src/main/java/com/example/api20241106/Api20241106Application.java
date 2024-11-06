4f2e231d1d9afff9dff521f1ff50d3402445915c
yujihun <sk0253642@gmail.com>
2024-11-06 10:03:14 +0900

    first

---
+++ src/main/java/com/example/api20241106/Api20241106Application.java
@@ -0,0 +1,13 @@
+package com.example.api20241106;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api20241106Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api20241106Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api20241106/Api20241106ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api20241106;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api20241106ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
