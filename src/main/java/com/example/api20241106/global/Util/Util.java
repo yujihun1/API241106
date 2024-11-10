6c7cb82c8fed2054217a76f22013ed7c5b49ef96
yujihun <sk0253642@gmail.com>
2024-11-10 14:36:39 +0900

    jwt add

---
+++ src/main/java/com/example/api20241106/global/Util/Util.java
@@ -0,0 +1,26 @@
+package com.example.api20241106.global.Util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Util {
+    public static class json {
+        public static Object toStr(Map<String, Object> map) {
+            try {
+                return new ObjectMapper().writeValueAsString(map);
+            } catch (JsonProcessingException e) {
+                return null;
+            }
+        }
+    }
+    public static Map<String, Object> toMap(String jsonStr) {
+        try {
+            return new ObjectMapper().readValue(jsonStr, LinkedHashMap.class);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/example/api20241106/global/jwt/JwtProvider.java
@@ -0,0 +1,75 @@
+package com.example.api20241106.global.jwt;
+
+import com.example.api20241106.global.Util.Util;
+import com.example.api20241106.member.entity.Member;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtProvider {
+    @Value("${custom.jwt.secretKey")
+    private String secretKeyOrigin;
+    private SecretKey cachedSecretKey;
+    // 시크릿키 가지고 오기
+    public SecretKey getSecretKey() {
+        if (cachedSecretKey == null) cachedSecretKey = _getSecretKey();
+        return cachedSecretKey;
+    }
+    // 스크릿 키 인코딩
+    private SecretKey _getSecretKey() {
+        String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyOrigin.getBytes());
+        return Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+    }
+    // refreshToken 토큰 만들기
+    public String genRefreshToken(Member member) {
+        return genToken(member, 60 * 60 * 24 * 365 * 1);
+    }
+    // accessToken 만들기
+    public String genAccessToken(Member member) {
+        return genToken(member, 60 * 10);
+    }
+    // 토큰 생성
+    public String genToken (Member member, int seconds) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", member.getId());
+        claims.put("username", member.getUsername());
+        long now = new Date().getTime();
+        Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+        return Jwts.builder()
+                .claim("body", Util.json.toStr(claims))
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+    // 토큰 유효성 검증
+    public boolean verify(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSecretKey())
+                    .build()
+                    .parseClaimsJws(token);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+    // 클레임 정보 받아오기
+    public Map<String, Object> getClaims(String token) {
+        String body = Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("body", String.class);
+        return Util.toMap(body);
+    }
+}
